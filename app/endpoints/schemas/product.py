99cb011e7a1a079c3494dae1120cc89a0ccbb8fb
Sergey Zorych <sergey.zorych@gmail.com>
2024-08-06 17:38:25 +0300

    FEAT [localisation]: add localisation

--- app/endpoints/schemas/product.py
+++ app/endpoints/schemas/product.py
@@ -1,10 +1,34 @@
-from typing import List
+from typing import List, Type, Any, Union, Dict
+
+from sqlmodel.main import _TSQLModel
 
 from app.endpoints.schemas.category import CategoryGetSchema
 from app.endpoints.schemas.ingredient import IngredientGetSchema
 from app.models.product import ProductReadBase
+from app.utils.translation.wrapper import translated
 
 
 class ProductGetSchema(ProductReadBase):
     category: CategoryGetSchema
     ingredients: List[IngredientGetSchema]
+
+    @classmethod
+    def model_validate(
+            cls: Type[_TSQLModel],
+            obj: Any,
+            *,
+            strict: Union[bool, None] = None,
+            from_attributes: Union[bool, None] = None,
+            context: Union[Dict[str, Any], None] = None,
+            update: Union[Dict[str, Any], None] = None,
+    ) -> _TSQLModel:
+        validated = super().model_validate(
+            obj=obj,
+            strict=strict,
+            from_attributes=from_attributes,
+            context=context,
+            update=update,
+        )
+        validated.name = translated(validated.name)
+        validated.description = translated(validated.description)
+        return validated
